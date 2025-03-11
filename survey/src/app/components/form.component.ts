68e9f2230e48f82dfd36b340e945d80a63a02c89
michael davis <maiku1davis@gmail.com>
2025-03-11 04:23:37 -0400

    pracitcing tempalte form

--- survey/src/app/components/form.component.ts
+++ survey/src/app/components/form.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { AsyncPipe, CommonModule, DatePipe, NgFor, NgIf } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { shareReplay, Subject, switchMap } from 'rxjs';
 import { injectTrpcClient } from '../../trpc-client';
 import { newfield } from '../models/newField';
@@ -8,6 +8,7 @@ import { newfield } from '../models/newField';
 @Component({
   selector: 'dynamic-form',
   imports: [AsyncPipe, FormsModule, NgFor, DatePipe, NgIf, CommonModule],
+  standalone: true,
   host: {
     class:
       'flex min-h-screen flex-col text-zinc-900 bg-zinc-50 px-4 pt-8 pb-32',
@@ -18,14 +19,16 @@ import { newfield } from '../models/newField';
     <form
       class="mt-8 pb-2 flex items-center flex-col gap-2"
       #f="ngForm"
-      (ngSubmit)="submit()"
+      (ngSubmit)="submit(f)"
     >
+      <button type="submit" [disabled]="!f.form.valid">Submit</button>
       <section *ngFor="let newField of newForm; trackBy: getId">
         <input
           required
           autocomplete="off"
-          name="dynForm"
-          [(ngModel)]="newForm[newField.id]"
+          [name]="newField.fieldName"
+          [(ngModel)]="newField.fieldName"
+          [ngModelOptions]="{standalone: true}"
           class="w-full inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background border border-input hover:text-zinc-950 h-11 px-2 rounded-md"
         />
       </section>
@@ -64,6 +67,7 @@ export class DynamicForm {
   );
   public surveyFields!: newfield[]
   public newForm: newfield[] = [{ id: this.generateRandomId(), fieldName: '' }]
+  public form!: NgForm
 
   newField() {
     this.newForm.push({ id: this.generateRandomId(), fieldName: '' })
@@ -75,9 +79,15 @@ export class DynamicForm {
   getformDatalength() {
     return this.formData.length
   }
-  submit() { }
+  submit(form: NgForm) {
+    if (form.valid)
+      console.log(this.newForm)
+  }
 
   generateRandomId(): number {
     return Math.floor(Math.random() * 1000000); // Adjust the range as needed
+  }
+  getModelToUse(theField: newfield) {
+    return this.newForm.find(n => n.id === theField.id)
   }
 }
